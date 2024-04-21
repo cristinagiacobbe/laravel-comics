ac26b4068b885643065c3055ccf5366e148c5696
cristinagiacobbe <crigiak@gmail.com>
2024-04-21 18:06:29 +0200

    Improve css

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,19 +1,77 @@
 <header>
+    <div class="full-nav">
 
-    <nav>
-        <img src="{{Vite::asset('resources/images/dc-logo.png')}}" alt="">
-        <ul>
-            <li>CHARACTERS</li>
-            <li>COMICS</li>
-            <li>MOVIES</li>
-            <li>TV</li>
-            <li>GAMES</li>
-            <li>COLLECTIBLES</li>
-            <li>VIDEOS</li>
-            <li>FANS</li>
-            <li>NEWS</li>
-            <li>SHOP</li>
+        <ul class="horizontal-bar">
+            <li class="nav-item">
+                <a class="nav-link" href="#">DC POWER VISA</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    ADDITIONAL DC SITES
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Something</a></li>
+                    <li><a class="dropdown-item" href="#">Something else</a></li>
+                </ul>
+            </li>
         </ul>
+    </div>
+
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <img src="{{Vite::asset('resources/images/dc-logo.png')}}" alt="" width="50" height="50">
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">CHARACTERS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">COMICS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">MOVIES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">TV</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">COLLECTIBLES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">VIDEOS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">COMICS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">FANS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">NEWS</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            SHOP
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Something</a></li>
+                            <li><a class="dropdown-item" href="#">Something else</a></li>
+
+                        </ul>
+                    </li>
+                </ul>
+                <span class="navbar-text">
+                    Search
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </span>
+            </div>
+        </div>
     </nav>
+
+    <div class="jumbotron"></div>
 
 </header>
